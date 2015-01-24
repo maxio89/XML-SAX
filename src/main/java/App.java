9e641055a1b2da9b7035b838a93617df75e177b6
Piotr Kozlowski <piotr.kozlowski@itcrowd.pl>
2015-01-24 16:54:44 +0100

    JAXB part 1

---
+++ src/main/java/App.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2006 Sun Microsystems, Inc.  All rights reserved.  U.S.
+ * Government Rights - Commercial software.  Government users are subject
+ * to the Sun Microsystems, Inc. standard license agreement and
+ * applicable provisions of the FAR and its supplements.  Use is subject
+ * to license terms.
+ *
+ * This distribution may include materials developed by third parties.
+ * Sun, Sun Microsystems, the Sun logo, Java and J2EE are trademarks
+ * or registered trademarks of Sun Microsystems, Inc. in the U.S. and
+ * other countries.
+ *
+ * Copyright (c) 2006 Sun Microsystems, Inc. Tous droits reserves.
+ *
+ * Droits du gouvernement americain, utilisateurs gouvernementaux - logiciel
+ * commercial. Les utilisateurs gouvernementaux sont soumis au contrat de
+ * licence standard de Sun Microsystems, Inc., ainsi qu'aux dispositions
+ * en vigueur de la FAR (Federal Acquisition Regulations) et des
+ * supplements a celles-ci.  Distribue par des licences qui en
+ * restreignent l'utilisation.
+ *
+ * Cette distribution peut comprendre des composants developpes par des
+ * tierces parties. Sun, Sun Microsystems, le logo Sun, Java et J2EE
+ * sont des marques de fabrique ou des marques deposees de Sun
+ * Microsystems, Inc. aux Etats-Unis et dans d'autres pays.
+ */
+
+import jaxb.Pliki;
+import jaxb.Zawartosc;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
+
+public class App {
+
+    private static final String RESOURCES_DIR = "src/main/resources/";
+
+    public static void main(String[] argv)
+    {
+        try {
+
+            File inFile = new File(RESOURCES_DIR + "in.xml");
+
+            JAXBContext jaxbContext = null;
+
+            jaxbContext = JAXBContext.newInstance(Pliki.class);
+
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+            Pliki pliki = (Pliki) jaxbUnmarshaller.unmarshal(inFile);
+
+            for (Zawartosc zawartosc : pliki.getZawartosc()) {
+
+                File includedFile = new File(RESOURCES_DIR + zawartosc.getPlik());
+                Zawartosc includedZawartosc = (Zawartosc) jaxbUnmarshaller.unmarshal(includedFile);
+
+                zawartosc = includedZawartosc;
+            }
+
+            System.out.println(pliki);
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/jaxb/Zawartosc.java
@@ -0,0 +1,65 @@
+
+package jaxb;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.adapters.CollapsedStringAdapter;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;attribute name="plik" use="required" type="{http://www.w3.org/2001/XMLSchema}NCName" />
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "")
+@XmlRootElement(name = "zawartosc")
+public class Zawartosc {
+
+    @XmlAttribute(name = "plik", required = true)
+    @XmlJavaTypeAdapter(CollapsedStringAdapter.class)
+    @XmlSchemaType(name = "NCName")
+    protected String plik;
+
+    /**
+     * Gets the value of the plik property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getPlik() {
+        return plik;
+    }
+
+    /**
+     * Sets the value of the plik property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setPlik(String value) {
+        this.plik = value;
+    }
+
+}
